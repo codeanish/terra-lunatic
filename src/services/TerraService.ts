29ddb2885485f5316dd5acc359d826be3620c1ce
Anish <anish@codeanish.com>
2021-11-02 00:09:39 +0000

    Default URL in case env is missing

--- src/services/TerraService.ts
+++ src/services/TerraService.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { ChallengeScore } from "../shared/types";
 
+const url = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:5000'
+
 const getChallengeScores = (address: string) => {
     return axios.get<ChallengeScore[]>
-    (process.env.REACT_APP_API_URL + `/address/${address}/scores`)
+    (url + `/address/${address}/scores`)
 }
 
 const getChallengeCateogies = () => {
     return axios.get<ChallengeScore[]>
-    (process.env.REACT_APP_API_URL + `/scores`)
+    (url + `/scores`)
 }
 
 const TerraService = {
